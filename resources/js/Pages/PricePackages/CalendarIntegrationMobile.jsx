7d53006b6f3d5993ea9c07ec8029161d7cbd8807
Udaya Adhikari <adhikariudaya521@gmail.com>
2026-05-10 10:46:19 +0545

    Make changes on the project

--- resources/js/Pages/PricePackages/CalendarIntegrationMobile.jsx
+++ resources/js/Pages/PricePackages/CalendarIntegrationMobile.jsx
@@ -1048,7 +1048,7 @@ const CalendarIntegrationMobile = ({ price }) => {
                                 htmlFor="address"
                                 className="block text-sm font-medium text-gray-700 mb-2"
                             >
-                                Address *
+                                Area *
                             </label>
                             <div className="relative">
                                 <Home className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400 pointer-events-none" />
@@ -1065,7 +1065,7 @@ const CalendarIntegrationMobile = ({ price }) => {
                                     }`}
                                 >
                                     <option value="">
-                                        Select your Address
+                                        Select your Area
                                     </option>
                                     <option value="mandurah">Mandurah</option>
                                     <option value="meadow-springs">
