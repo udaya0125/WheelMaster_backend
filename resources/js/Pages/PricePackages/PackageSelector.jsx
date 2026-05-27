712b2d6b458f2435420c0dece1ddec0bf0f91cd6
Udaya Adhikari <adhikariudaya521@gmail.com>
2026-05-27 16:17:23 +0545

    Change the text

--- resources/js/Pages/PricePackages/PackageSelector.jsx
+++ resources/js/Pages/PricePackages/PackageSelector.jsx
@@ -72,7 +72,7 @@ const PackageSelector = ({ price, packageOptions = [], className = "" }) => {
                 htmlFor="package-selector"
                 className="block text-sm font-medium text-gray-700 mb-2"
             >
-                Change package
+                Lesson Type
             </label>
             <div className="relative">
                 <Package className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400 pointer-events-none" />
