f2e86e589ed8e64dc47eb68a29b68e9dd8fff239
Udaya Adhikari <adhikariudaya521@gmail.com>
2026-08-10 13:26:38 +0545

    Fixed the routes

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('/dashboard');
+        return redirect()->intended('/');
     }
 
     /**
